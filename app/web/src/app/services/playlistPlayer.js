bec1cb8284a37a1d9d555d9ab60fe6b5fc460bec
Alf <alf.g.jr@gmail.com>
2015-07-22 22:50:06 -0700

    Fix YT loading bug. Add 'playing' icon when playing a playlist.

--- app/web/src/app/services/playlistPlayer.js
+++ app/web/src/app/services/playlistPlayer.js
@@ -10,245 +10,252 @@ _.templateSettings = {
 }
 
 export default class PlaylistPlayer {
-        constructor(options) {
-            // super(options);
+	constructor(options) {
+		// super(options);
 
-    		this._defaults = {
-    			player: {
-    				quality: 'hd720',
-    				autohide: 1
-    			},
-                getCategoriesApi: '/api/categories',
-                getCategoryByIdApi: '/api/categories/{0}',
-                getPlaylistsApi: '/api/playlists',
-                getNewReleasesApi: '/api/new-releases',
-                getUserPlaylistsApi: '/api/playlists/{0}',
-                getPlaylistTracksByIdApi: '/api/playlists/{0}/{1}/tracks',
+		this._defaults = {
+			player: {
+				quality: 'hd720',
+				autohide: 1
+			},
+			getCategoriesApi: '/api/categories',
+			getCategoryByIdApi: '/api/categories/{0}',
+			getPlaylistsApi: '/api/playlists',
+			getNewReleasesApi: '/api/new-releases',
+			getUserPlaylistsApi: '/api/playlists/{0}',
+			getPlaylistTracksByIdApi: '/api/playlists/{0}/{1}/tracks',
 
-                userPlaylistTemplate: '/static/tmpl/userPlaylist.html',
-                playlistTemplate: '/static/tmpl/playlists.html',
-                categoriesTemplate: '/static/tmpl/categories.html'
-    		};
+			userPlaylistTemplate: '/static/tmpl/userPlaylist.html',
+			playlistTemplate: '/static/tmpl/playlists.html',
+			categoriesTemplate: '/static/tmpl/categories.html'
+		};
 
-    		this._options = $.extend(true, {}, this._defaults, options);
+		this._options = $.extend(true, {}, this._defaults, options);
 
-    		this._player = window._player; // Attach to window to persist session.
-            this._playlists = null;
-            this._categories = null;
+		this._playlists = null;
+		this._categories = null;
+		this._player = window._player;
 
-            this.youTubeLoadPlayer(); // Loads Youtube iFrame Player.
-        }
+		this.init();
+	}
 
-        init() {
-            this.getFeaturedPlaylists();
-        }
+	init() {
+		this.youTubeLoadPlayer(); // Loads Youtube iFrame Player.
+	}
 
-		buildVideosArray(tracksArr) {
-			var tracks = tracksArr.data.items;
-			var videos = [];
+	buildVideosArray(tracksArr) {
+		var tracks = tracksArr.data.items;
+		var videos = [];
 
-			for (var i = 0; i < tracks.length; i++) {
+		for (var i = 0; i < tracks.length; i++) {
 
-				var v = tracks[i].track.external_ids.youtube;
-				videos.push(v);
-			}
-
-			return videos;
+			var v = tracks[i].track.external_ids.youtube;
+			videos.push(v);
 		}
 
-        getFeaturedPlaylists() {
-            var self = this;
+		return videos;
+	}
 
-			$('#playlists').removeClass('show');
+	getFeaturedPlaylists() {
+		var self = this;
 
-			$.ajax({
-				url: self._options.getPlaylistsApi,
-				dataType: 'json'
-			})
-            .done(function(data) {
-                self._playlists = data;
-                self.renderPlaylists(data);
-            });
-        }
+		$('#playlists').removeClass('show');
 
-        getCategories() {
-            var self = this;
+		$.ajax({
+			url: self._options.getPlaylistsApi,
+			dataType: 'json'
+		})
+		.done(function(data) {
+			self._playlists = data;
+			self.renderPlaylists(data);
+		});
+	}
 
-			$('#playlists').removeClass('show');
+	getCategories() {
+		var self = this;
 
-			$.ajax({
-				url: self._options.getCategoriesApi,
-				dataType: 'json'
-			})
-            .done(function(data) {
-                self._categories = data;
-                self.renderCategories(data);
-            });
-        }
+		$('#playlists').removeClass('show');
 
-        getCategoryById(category) {
-            var self = this;
+		$.ajax({
+			url: self._options.getCategoriesApi,
+			dataType: 'json'
+		})
+		.done(function(data) {
+			self._categories = data;
+			self.renderCategories(data);
+		});
+	}
 
-			$('#playlists').removeClass('show');
+	getCategoryById(category) {
+		var self = this;
 
-			$.ajax({
-				url: String.format(self._options.getCategoryByIdApi, category),
-				dataType: 'json'
-			})
-            .done(function(data) {
-                self._playlists = data;
-                self.renderPlaylists(data);
-            });
-        }
+		$('#playlists').removeClass('show');
 
-        getUserPlaylists(user) {
-            var self = this;
+		$.ajax({
+			url: String.format(self._options.getCategoryByIdApi, category),
+			dataType: 'json'
+		})
+		.done(function(data) {
+			self._playlists = data;
+			self.renderPlaylists(data);
+		});
+	}
 
-			$('#playlists').removeClass('show');
+	getUserPlaylists(user) {
+		var self = this;
 
-			$.ajax({
-				url: String.format(self._options.getUserPlaylistsApi, user),
-				dataType: 'json'
-			})
-            .done(function(data) {
-                self._playlists = data;
-                self.renderPlaylists(data);
-            });
+		$('#playlists').removeClass('show');
 
-        }
+		$.ajax({
+			url: String.format(self._options.getUserPlaylistsApi, user),
+			dataType: 'json'
+		})
+		.done(function(data) {
+			self._playlists = data;
+			self.renderPlaylists(data);
+		});
 
-        clickPlaylist(e, el) {
-            var self = this;
+	}
 
-            e.preventDefault(); // Prevent updating to index (#) route.
+	clickPlaylist(e, el) {
+		var self = this;
 
-            var playlist = $(el).data('playlist');
-            self.loadPlaylist(playlist);
-			$('.js-spinner').text('Loading...');
-			window.scrollTo(0, 0);
-        }
+		$('#playlists .playing').removeClass('on');
+		$('#playlists img').removeClass('blur');
+		$(el).find('.playing').addClass('on');
+		$(el).find('img').addClass('blur');
 
-        loadPlaylist(playlistName) {
-            var self = this;
-			console.log(this._player);
+		e.preventDefault(); // Prevent updating to index (#) route.
 
-            var playlist = _.find(self._playlists.data, function(p) {
-                return p.name == playlistName
-            });
+		var playlist = $(el).data('playlist');
+		self.loadPlaylist(playlist);
+		$('.js-spinner').text('Loading...');
+		window.scrollTo(0, 0);
+	}
 
-			var url = String.format(self._options.getPlaylistTracksByIdApi,
-                playlist.owner.id, playlist.id);
+	loadPlaylist(playlistName) {
+		var self = this;
+
+		var playlist = _.find(self._playlists.data, function(p) {
+			return p.name == playlistName
+		});
+
+		var url = String.format(self._options.getPlaylistTracksByIdApi,
+			playlist.owner.id, playlist.id);
 
 			$.ajax({
 				url: url,
 				dataType: 'json'
 			})
-            .done(function(data) {
+			.done(function(data) {
 				var videos = self.buildVideosArray(data);
 				self._player.loadPlaylist(videos, 0, 5, self._options.player.quality);
 				$('.js-spinner').empty();
 				$('#player').slideDown();
-            });
-        }
-
-        renderPlaylists(data) {
-            var self = this;
-            var playlistData = data.data;
-
-            $.ajax({
-                url: self._options.playlistTemplate,
-                dataType: 'html'
-            })
-            .done(function(tmpl) {
-
-				var playlistTmpl = _.template(tmpl);
-
-				$('#playlists')
-					.html(playlistTmpl({data: playlistData}))
-					.addClass('show');
-                $('.js-playlist').on('click', function(e, el) { self.clickPlaylist(e, this) });
-            });
-        }
-
-        renderUserPlaylist() {
-            var self = this;
-
-            $.ajax({
-                url: self._options.userPlaylistTemplate,
-                dataType: 'html'
-            })
-            .done(function(tmpl) {
-
-				var userPlaylistTmpl = _.template(tmpl);
-
-				$('#playlists')
-					.html(userPlaylistTmpl)
-					.addClass('show');
-
-				$('#user-form').on('submit', function(e) {
-					e.preventDefault();
-
-					var user = $('#user-input').val();
-					window.location.hash = `/playlist/${user}`;
-				});
-            });
-        }
-
-        renderCategories(data) {
-            var self = this;
-            var categoriesData = data.data;
-
-            $.ajax({
-                url: self._options.categoriesTemplate,
-                dataType: 'html'
-            })
-            .done(function(tmpl) {
-
-				var categoriesTmpl = _.template(tmpl);
-
-				$('#playlists')
-					.html(categoriesTmpl({data: categoriesData}))
-					.addClass('show');
-            });
-        }
-
-		player_Ready(e) {
-			// e.target.playVideo();
-		}
-
-		youTubeLoadPlayer() {
-
-            var self = this;
-			// Setup ready event callback for YouTube iframe API.
-			window.onYouTubeIframeAPIReady 	= function() { self.youTube_IframeAPIReady(self._options); };
-
-			// Load the YouTube Iframe API.
-			var $script = $('<script />', {
-				src: 'https://www.youtube.com/iframe_api'
 			});
+	}
 
-			$('script')
-				.first()
-				.before($script)
-			;
-		}
+	renderPlaylists(data) {
+		var self = this;
+		var playlistData = data.data;
 
-		youTube_IframeAPIReady(options) {
+		$.ajax({
+			url: self._options.playlistTemplate,
+			dataType: 'html'
+		})
+		.done(function(tmpl) {
 
-			window._player = new YT.Player('player', {
+			var playlistTmpl = _.template(tmpl);
 
-				playerVars: {
-					autohide: 		options.player.autohide,
-					autoplay: 		0,
-					iv_load_policy: 3,
-					modestbranding: 0,
-					showinfo: 	1
-				},
+			$('#playlists')
+			.html(playlistTmpl({data: playlistData}))
+			.addClass('show');
+			$('.js-playlist').on('click', function(e, el) { self.clickPlaylist(e, this) });
+		});
+	}
 
-				events: {
-					'onReady': 			this.player_Ready,
-					'onStateChange': 	this.player_StateChange
-				}
+	renderUserPlaylist() {
+		var self = this;
+
+		$.ajax({
+			url: self._options.userPlaylistTemplate,
+			dataType: 'html'
+		})
+		.done(function(tmpl) {
+
+			var userPlaylistTmpl = _.template(tmpl);
+
+			$('#playlists')
+			.html(userPlaylistTmpl)
+			.addClass('show');
+
+			$('#user-form').on('submit', function(e) {
+				e.preventDefault();
+
+				var user = $('#user-input').val();
+				window.location.hash = `/playlist/${user}`;
 			});
-		}
-    }
+		});
+	}
+
+	renderCategories(data) {
+		var self = this;
+		var categoriesData = data.data;
+
+		$.ajax({
+			url: self._options.categoriesTemplate,
+			dataType: 'html'
+		})
+		.done(function(tmpl) {
+
+			var categoriesTmpl = _.template(tmpl);
+
+			$('#playlists')
+			.html(categoriesTmpl({data: categoriesData}))
+			.addClass('show');
+		});
+	}
+
+	player_Ready(e) {
+		var self = this;
+
+		this._player = window._player;
+	}
+
+	youTubeLoadPlayer() {
+
+		var self = this;
+		// Setup ready event callback for YouTube iframe API.
+		window.onYouTubeIframeAPIReady = function() { self.youTube_IframeAPIReady(self._options); };
+
+		// Load the YouTube Iframe API.
+		var $script = $('<script />', {
+			src: 'https://www.youtube.com/iframe_api'
+		});
+
+		$('script')
+		.first()
+		.before($script)
+		;
+	}
+
+	youTube_IframeAPIReady(options) {
+		var self = this;
+
+		window._player = new YT.Player('player', {
+
+			playerVars: {
+				autohide: 		options.player.autohide,
+				autoplay: 		0,
+				iv_load_policy: 3,
+				modestbranding: 0,
+				showinfo: 	1
+			},
+
+			events: {
+				'onReady': 			function() { self.player_Ready() },
+				'onStateChange': 	this.player_StateChange
+			}
+		});
+	}
+}
