f913fcb5527b5ecd84f2ed5239cd499d68d71e92
Alf <alf.g.jr@gmail.com>
2015-07-28 20:51:28 -0700

    Update config settings to use env variables.

---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ * All global configuration.
+ */
+
+var config = {
+    port: process.env.PORT || 3000,
+    client_id: process.env.CLIENT_ID || '',
+    client_secret: process.env.CLIENT_SECRET || '',
+    redirect_uri: process.env.REDIRECT_URI || 'http://localhost:8888/callback',
+    googleApiKey: process.env.GOOGLE_API_KEY || '',
+}
+
+module.exports = config;
